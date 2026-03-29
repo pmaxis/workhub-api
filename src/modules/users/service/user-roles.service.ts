050f74646babf011cf957d263b5cb20efae1ae9a
Maksym Pshenychnyi <p.maxis0601@gmail.com>
2026-03-29 11:53:56 +0300

    Fixed user roles

--- src/modules/users/service/user-roles.service.ts
+++ src/modules/users/service/user-roles.service.ts
@@ -11,18 +11,27 @@ export class UserRolesService {
   ) {}
 
   async addRole(userId: string, roleId: string): Promise<void> {
-    const role = await this.rolesRepository.findByIdForCheck(roleId);
-    if (role?.slug === ADMIN_ROLE_SLUG) {
+    const isAdmin = await this.isRoleAdmin(roleId);
+
+    if (isAdmin) {
       throw new BadRequestException('Cannot assign reserved role');
     }
+
     await this.userRolesRepository.addRole({ userId, roleId });
   }
 
   async deleteRole(userId: string, roleId: string): Promise<void> {
-    const role = await this.rolesRepository.findByIdForCheck(roleId);
-    if (role?.slug === ADMIN_ROLE_SLUG) {
+    const isAdmin = await this.isRoleAdmin(roleId);
+
+    if (isAdmin) {
       throw new BadRequestException('Cannot remove reserved role');
     }
+
     await this.userRolesRepository.deleteRole(userId, roleId);
+  }
+
+  private async isRoleAdmin(roleId: string): Promise<boolean> {
+    const role = await this.rolesRepository.findByIdForCheck(roleId);
+    return role?.slug === ADMIN_ROLE_SLUG;
   }
 }
